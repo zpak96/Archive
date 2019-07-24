76d940b77dcd904a6596028dd69db940238c02ff
Zane Paksi <32249913+zpak96@users.noreply.github.com>
2019-07-23 22:35:08 -0400

    Binary to Quaternary converter
    Created this for a personal project, and may use this again later.

---
+++ 150/bi2quat.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+
+def main():
+    quat = {"00":"0", "01":"1", "10":"2", "11":"3"}
+
+    print(":: Enter 'q' to exit ::")
+
+    usrinput = str(input("Binary to Quaternary:: "))
+
+    while usrinput != 'q':
+        print("Your Entry: " + usrinput)
+
+        #make the binary string even by adding a leading zero
+        if len(usrinput) % 2 != 0:
+            usrinput = "0" + usrinput
+            print(usrinput)
+
+        #maybe revisit and try to do this through a lambda?
+	#it would be pretty cool.
+        #cool = list(map(lambda x,y : x[], usrinput)
+
+        #convert another or exit
+        usrinput = str(input("Binary to Quaternary:: "))
+
+main()
